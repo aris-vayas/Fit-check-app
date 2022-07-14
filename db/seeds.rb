1bd3afc3d585384d76b52d0fe3b5680e3ba8de10
Aris Vayas <aris.vayas@gmail.com>
2022-07-14 09:48:35 -0400

    herkou ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,15 @@
 
 puts "seeding users..."
 
-User.create(full_name: 'third user', email: 'arisv428@gmail.com', password: 'secret', username: 'arisv428@gmail.com')
+User.create!(full_name: 'Aris Vayas', email: 'arisv428@gmail.com', password: 'secret', username: 'arisv428@gmail.com')
+User.create!(full_name: 'Bob Villa', email: 'aris.vayas@gmail.com', password: 'secret', username: 'aris.vayas@gmail.com')
 
 puts "seeing photos"
 
 
- 50.times do
+ 25.times do
 
+    Photo.create!(image:Unsplash::Photo.random(query:"shoes")[:urls][:regular], score: 0, user: User.first )
     Photo.create!(image:Unsplash::Photo.random(query:"shoes")[:urls][:regular], score: 0, user: User.last )
 
 
